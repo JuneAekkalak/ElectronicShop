62e4c1eef70d08d75d18146538bf06fe5e10ae35
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2022-12-04 18:46:09 +0700

    fixing indent and add emoji

--- frontend/views/site/all-product.php
+++ frontend/views/site/all-product.php
@@ -23,8 +23,10 @@ if (isset($_GET['product_name'])) {
 
 <section style="margin: 0px 0">
     <div class="container">
-        <div class="my-3">
-            <label>ค้นหาสินค้า</label>
+        <div class="my-5">
+            <label>
+                <h2>ค้นหาสินค้า 🔍</h2>
+            </label>
             <div class="d-flex">
                 <input type="text" id="productName" class="form-control" value="<?php echo isset($_GET['product_name']) ? $_GET['product_name'] : null; ?>">
                 <button class="btn btn-primary ml-2" onclick="searchProduct()">Search</button>
@@ -42,9 +44,9 @@ if (isset($_GET['product_name'])) {
         </div>
         <div class="row">
             <?php
-                if($searched) {
-                    foreach ($products as $index => $model) { ?>
-                        <div class="col-md-3 col-6" style="margin-bottom: 4em;">
+            if ($searched) {
+                foreach ($products as $index => $model) { ?>
+                    <div class="col-md-3 col-6" style="margin-bottom: 4em;">
                         <!-- card item -->
                         <div class="single_product_model">
                             <div style="height: 220px;">
@@ -69,39 +71,39 @@ if (isset($_GET['product_name'])) {
                         </div>
                         <!-- end of card item -->
                     </div>
-                    <?php }
-                } else {
-                    foreach ($products as $index => $model) { ?>
-                        <div class="col-md-3 col-6" style="margin-bottom: 4em;">
-                            <!-- card item -->
-                            <div class="single_product_model">
-                                <div style="height: 220px;">
-                                    <img src="<?= $model->productImage[1] ?>" alt="">
-                                </div>
-                                <div class="single_product_text mt-5" style="height: 220px;">
-                                    <h4><?= $model->productName ?></h4>
-                                    <?php $brandName = Brand::find()->where(['brand_id' => $model->brand_id])->one()->brandName; ?>
-                                    <p><?= $brandName ?></p>
-                                    <div class="d-flex justify-content-between">
-                                        <b style="color: #F1574F;">
-                                            ฿ <?= number_format($model->productPrice) ?>
-                                        </b>
-                                        <!-- price before discount -->
-                                        <b style="color: #BDBDBD; text-decoration: line-through;">
-                                            ฿ <?= number_format($model->productPrice + 2000) ?>
-                                        </b>
-                                    </div>
-                                    <p><?= strlen($model->productDescrip) > 50 ? mb_substr($model->productDescrip, 0, 50, 'UTF-8') . "..." : " " ?></p>
-                                </div>
-                                <a href="index.php?r=products/view&_id=<?= $model->product_id ?>" class="btn btn-warning btn-sm btn-block mt-2">More Detail</a>
+                <?php }
+            } else {
+                foreach ($products as $index => $model) { ?>
+                    <div class="col-md-3 col-6" style="margin-bottom: 4em;">
+                        <!-- card item -->
+                        <div class="single_product_model">
+                            <div style="height: 220px;">
+                                <img src="<?= $model->productImage[1] ?>" alt="">
                             </div>
-                            <!-- end of card item -->
+                            <div class="single_product_text mt-5" style="height: 220px;">
+                                <h4><?= $model->productName ?></h4>
+                                <?php $brandName = Brand::find()->where(['brand_id' => $model->brand_id])->one()->brandName; ?>
+                                <p><?= $brandName ?></p>
+                                <div class="d-flex justify-content-between">
+                                    <b style="color: #F1574F;">
+                                        ฿ <?= number_format($model->productPrice) ?>
+                                    </b>
+                                    <!-- price before discount -->
+                                    <b style="color: #BDBDBD; text-decoration: line-through;">
+                                        ฿ <?= number_format($model->productPrice + 2000) ?>
+                                    </b>
+                                </div>
+                                <p><?= strlen($model->productDescrip) > 50 ? mb_substr($model->productDescrip, 0, 50, 'UTF-8') . "..." : " " ?></p>
+                            </div>
+                            <a href="index.php?r=products/view&_id=<?= $model->product_id ?>" class="btn btn-warning btn-sm btn-block mt-2">More Detail</a>
                         </div>
-                    <?php }
-                }
+                        <!-- end of card item -->
+                    </div>
+            <?php }
+            }
             ?>
 
-            
+
         </div>
     </div>
 </section>
