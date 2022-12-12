14536b58c28ca0fac62f9d9b0b98425c443b784f
JuneAekkalak <aekkalak.ju@gmail.com>
2022-12-12 17:52:06 +0700

    perminsion

--- backend/controllers/OrderController.php
+++ backend/controllers/OrderController.php
@@ -7,6 +7,7 @@ use app\models\OrderSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii;
 
 /**
  * OrderController implements the CRUD actions for Order model.
@@ -38,13 +39,17 @@ class OrderController extends Controller
      */
     public function actionIndex()
     {
-        $searchModel = new OrderSearch();
-        $dataProvider = $searchModel->search($this->request->queryParams);
+        if (Yii::$app->user->isGuest)
+            $this->redirect(['site/login']);
+        else {
+            $searchModel = new OrderSearch();
+            $dataProvider = $searchModel->search($this->request->queryParams);
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
+            return $this->render('index', [
+                'searchModel' => $searchModel,
+                'dataProvider' => $dataProvider,
+            ]);
+        }
     }
 
     /**
